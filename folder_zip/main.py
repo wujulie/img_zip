c1276160cf70c1e96e8372c223774794534fac64
Julie <49779496+wujulie@users.noreply.github.com>
2023-03-06 15:34:10 +0800

    Update main.py
    fix loop error

--- folder_zip/main.py
+++ folder_zip/main.py
@@ -7,37 +7,37 @@ from PIL import Image
 
 
 
-def img_zip(ori_path,new_path,full_path):
-    all_img_name = [f for f in os.listdir(ori_path) if os.path.isfile(os.path.join(ori_path, f))]  # 圖片名稱
+def img_zip(source_folder,target_folder):
     target_size = 80 * 1024  # 單位B
     quality = 90  # 保留質量，1-100，1 最差，100 最好，
     step = 10  # 降低質量的步長
-    for img in full_path:
-        current_img = Image.open(img)  # 初始化當前圖片為原圖
-        current_size = os.path.getsize(img)  # 初始化當前大小為原圖大小
+    for filename in os.listdir(source_folder):
+        if filename.endswith(".jpg") or filename.endswith(".png") or filename.endswith(".jpeg"):
+         current_img = Image.open(os.path.join(source_folder, filename))
+         current_size = os.path.getsize(current_img)  # 初始化當前大小為原圖大小
         while current_size > target_size and quality > 0:
-            for name in all_img_name:
-                current_img.save(new_path + "\\" + name, quality=quality,
-                                 optimize=True)  # 壓縮新圖片到new_img_path
-                current_img = Image.open(new_path + "\\" + name)  # 更新 current_img 為壓縮後的圖片
-                # 更新 current_size 為壓縮後新圖片大小
-                current_size = os.path.getsize(new_path + "\\" + name)
-                quality -= step
+            current_img.save(os.path.join(target_folder, filename), 
+                             optimize=True,quality=quality)
+                          # 壓縮新圖片到new_img_path
+            current_img = Image.open(os.path.join(source_folder, filename))  # 更新 current_img 為壓縮後的圖片
+            # 更新 current_size 為壓縮後新圖片大小
+            current_size = os.path.getsize(os.path.join(source_folder, filename))
+            quality -= step
+
+
 def main():
     print("請選擇你的原始圖片資料夾\n")
     root = tk.Tk()
     root.withdraw()
-    dirpath = filedialog.askdirectory()
-    full_path = glob.glob(os.path.join(dirpath, "*jpg"))
+    source_folder = filedialog.askdirectory()
     print("請選擇你的輸出圖片資料夾\n")
     output = tk.Tk()
     output.withdraw()
-    newpath = filedialog.askdirectory()
-    print(newpath)
-    img_zip(dirpath,newpath,full_path)
+    target_folder  = filedialog.askdirectory()
+    print(target_folder)
+    img_zip(source_folder,target_folder)
 
 
 
 if __name__ == '__main__':
     main()#待修復迴圈部分
-
